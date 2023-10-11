f94c5d92bb3655c71e6dbb5b2bf5a23e2e3a6220
Kyle <krgamer444@gmail.com>
2023-10-11 17:18:40 -0400

    v.05

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import mysql.connector as msql
 #)
 
 app = fl(__name__)
+app.secret_key = "Team3Project"
 
 
 @app.route("/")
@@ -21,7 +22,16 @@ def homepage():
     return render_template("index.html")
 
 @app.route("/order", methods = ["GET", "POST"])
-def placeOrder(order):
+def placeOrder():
+    if request.method == "POST":
+        flavor = request.form.get("flavor")
+        size = request.form.get("size")
+        quantity = request.form.get("quantity")
+        decor = request.form.get("decor")
+        pass
+    #TODO: Send data to database
+    #TODO: Actually set up database
+
     return render_template(url_for("order.html"))
 
 
