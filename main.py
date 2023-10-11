f01e7ccec3b320d6790d1630395f31edb1ad7479
Kyle <krgamer444@gmail.com>
2023-10-11 15:37:30 -0400

    Fixed formatting for FLASK

--- main.py
+++ main.py
@@ -2,25 +2,29 @@
 # This is the main driver for the project; hosts all methods, calls to database, as well as functionally runs the website.
 
 from flask import Flask as fl
-from flask import url_for, request, render_template
+from flask import url_for, request, render_template, redirect
 from markupsafe import escape
 import mysql.connector as msql
 
 #Database connection set up
-mydb = msql.connect(
-    host = "localhost",
-    user = "root",
-    password = "root"
-)
+#mydb = msql.connect(
+ #   host = "localhost",
+ #   user = "root",
+ #   password = "root"
+#)
 
 app = fl(__name__)
 
 
 @app.route("/")
 def homepage():
-    pass #TODO: Obtain index.html and display it upon launch of webapp.
+    return render_template("index.html")
 
 @app.route("/order", methods = ["GET", "POST"])
 def placeOrder(order):
-    pass #TODO: Obtain orderForm.html and add functionality.
+    return render_template(url_for("order.html"))
 
+
+
+if __name__ == '__main__':
+    app.run()
