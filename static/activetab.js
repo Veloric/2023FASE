cf96dea6e67e2d726fcf6bd321614ffc3b1777d7
Linda Mei <85714837+ilindaa@users.noreply.github.com>
2023-10-12 21:57:11 -0400

    Fixed the active tab navbar bug

---
+++ static/activetab.js
@@ -0,0 +1,14 @@
+// For order, index, and contact
+// Check if the location has a navbar item's name and if it does, set it as the active tab
+(function setActiveTab() {
+    let navbar = document.querySelector(".nav-bar");
+    let tab = window.location.href;
+
+    if (tab.includes("order")) {
+        navbar.children[0].firstChild.classList.add("active");
+    } else if (tab.includes("contact")) {
+        navbar.children[2].firstChild.classList.add("active");
+    } else {
+        navbar.children[1].firstChild.classList.add("active");
+    }
+})();
