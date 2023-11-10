cab63a7f74409d3bc0494eefdd36fb4e6a0612d1
Kaysey N. <85714834+kay-ne@users.noreply.github.com>
2023-11-09 19:32:17 -0500

    edits for menu

--- main.py
+++ main.py
@@ -123,7 +123,7 @@ def menu():
 @app.route("/addMenu", methods=["GET", "POST"])
 def addMenu():
     msg = ""
-    if request.method == "POST" and request.form["menuID"] == 1:
+    if request.method == "POST" and request.form["menuID"] == "1":
         menuID = request.form["menuID"]
         categoryName = request.form["categoryName"]
         dessertName = request.form["dessertName"]
@@ -135,7 +135,7 @@ def addMenu():
         print(cursor.rowcount, " record inserted!") # TESTING
         disconnectdb(mydb)
         msg = "Form received! You may now exit this page."
-    elif request.method == "POST" and request.form["menuID"] == 2:
+    elif request.method == "POST" and request.form["menuID"] == "2":
         menuID = request.form["menuID"]
         categoryName = request.form["categoryName"]
         sizeName = request.form["sizeName"]
@@ -148,7 +148,7 @@ def addMenu():
         print(cursor.rowcount, " record inserted!") # TESTING
         disconnectdb(mydb)
         msg = "Form received! You may now exit this page."
-    elif request.method == "POST" and request.form["menuID"] == 3:
+    elif request.method == "POST" and request.form["menuID"] == "3":
         menuID = request.form["menuID"]
         categoryName = request.form["categoryName"]
         PCName = request.form["PCName"]
@@ -160,7 +160,7 @@ def addMenu():
         print(cursor.rowcount, " record inserted!") # TESTING
         disconnectdb(mydb)
         msg = "Form received! You may now exit this page."
-    elif request.method == "POST" and request.form["menuID"] == 4:
+    elif request.method == "POST" and request.form["menuID"] == "4":
         menuID = request.form["menuID"]
         sizeName = request.form["sizeName"]
         sizeDescription = request.form["sizeDescription"]
@@ -189,7 +189,7 @@ def editMenu():
     cursor.execute("SELECT COUNT(*) FROM MiniDesserts")
     rowCount = cursor.fetchall()[0][0]
 
-    if request.method == "POST" and request.form["menuID"] == 1:
+    if request.method == "POST" and request.form["menuID"] == "1":
         miniDessertsID = request.form["miniDessertsID"]
         categoryName = request.form["categoryName"]
         dessertName = request.form["dessertName"]
@@ -198,7 +198,7 @@ def editMenu():
         mydb.commit()
         print(cursor.rowcount, " record updated!") # TESTING
         msg = "Form received! You may now exit this page."
-    elif request.method == "POST" and request.form["menuID"] == 2:
+    elif request.method == "POST" and request.form["menuID"] == "2":
         dessertTrayID = request.form["dessertTrayID"]
         categoryName = request.form["categoryName"]
         sizeName = request.form["sizeName"]
@@ -208,7 +208,7 @@ def editMenu():
         mydb.commit()
         print(cursor.rowcount, " record updated!") # TESTING
         msg = "Form received! You may now exit this page."
-    elif request.method == "POST" and request.form["menuID"] == 3:
+    elif request.method == "POST" and request.form["menuID"] == "3":
         PCID = request.form["PCID"]
         categoryName = request.form["categoryName"]
         PCName = request.form["PCName"]
@@ -217,7 +217,7 @@ def editMenu():
         mydb.commit()
         print(cursor.rowcount, " record updated!") # TESTING
         msg = "Form received! You may now exit this page."
-    elif request.method == "POST" and request.form["menuID"] == 4:
+    elif request.method == "POST" and request.form["menuID"] == "4":
         cupcakeID = request.form["cupcakeID"]
         sizeName = request.form["sizeName"]
         sizeDescription = request.form["sizeDescription"]
@@ -243,7 +243,7 @@ def deleteMenu():
     mydb = connectdb()
     cursor = mydb.cursor()
 
-    if request.method == "POST" and request.form["menuID"] == 1: 
+    if request.method == "POST" and request.form["menuID"] == "1": 
         print(request.form)
         miniDessertsID = request.form["miniDessertsID"]
 
@@ -258,19 +258,19 @@ def deleteMenu():
             msg = "Form received! You may now exit this page."
         else:
             msg = "Error, the ID is invalid!"
-    elif request.method == "POST" and request.form["menuID"] == 2:
+    elif request.method == "POST" and request.form["menuID"] == "2":
         dessertTrayID = request.form["dessertTrayID"]
         cursor.execute("DELETE from DessertTray WHERE DessertTrayID = %s", [(dessertTrayID)])
         mydb.commit()
         print(cursor.rowcount, " record deleted!") # TESTING
         msg = "Form received! You may now exit this page."
-    elif request.method == "POST" and request.form["menuID"] == 3:
+    elif request.method == "POST" and request.form["menuID"] == "3":
         PCID = request.form["PCID"]
         cursor.execute("DELETE from PieAndCheesecake WHERE PCID = %s", [(PCID)])
         mydb.commit()
         print(cursor.rowcount, " record deleted!") # TESTING
         msg = "Form received! You may now exit this page."
-    elif request.method == "POST" and request.form["menuID"] == 4:
+    elif request.method == "POST" and request.form["menuID"] == "4":
         cupcakeID = request.form["cupcakeID"]
         cursor.execute("DELETE from Cupcake WHERE CupcakeID = %s", [(cupcakeID)])
         mydb.commit()
--- static/gallery.js
+++ static/gallery.js
@@ -5,15 +5,15 @@ function showSlides() {
     let i;
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
-     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
     slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
+    if (slideIndex > slides.length) { slideIndex = 1 }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+        dots[i].className = dots[i].className.replace(" active-dot", "");
     }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides,5000); 
-    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active-dot";
+    setTimeout(showSlides, 5000);
+}
