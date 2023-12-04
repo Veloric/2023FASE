8636346e47836e2c96c4c4a07193fbd179953660
Linda Mei <85714837+ilindaa@users.noreply.github.com>
2023-12-03 19:26:39 -0500

    Update navbar and CSS

--- static/activetab.js
+++ static/activetab.js
@@ -2,19 +2,25 @@
 // Check if the location has a navbar item's name and if it does, set it as the active tab
 (function setActiveTab() {
     let navbar = document.querySelector(".nav-bar");
-    let tab = window.location.href;
+    let tab = window.location.pathname;
+    console.log(tab);
+    console.log(typeof(tab));
 
-    if (tab.includes("order")) {
+    if (tab === "/order") {
         addActive(navbar, 0);
-    } else if (tab.includes("galleryphotos")) {
+    } else if (tab === "/galleryphotos") {
         addActive(navbar, 2);
-    } else if (tab.includes("contact")) {
+    } else if (tab === "/contact") {
         addActive(navbar, 3);
-    } else if (tab.includes("menu")) {
+    } else if (tab === "/menu") {
         addActive(navbar, 4);
-    } else if (tab.includes("adminPage")) {
+    } else if (tab === ("/adminPage")) {
         addActive(navbar, 5);
-    } else {
+    } else if (tab === ("/login")) {
+        addActive(navbar, 6);
+    } else if (tab === ("/register")) {
+        addActive(navbar, 7);
+    } else if (tab === "/"){
         addActive(navbar, 1);
     }
 })();
--- static/alerts.js
+++ static/alerts.js
@@ -1,12 +1,16 @@
 (function addDismiss() {
     let close = document.querySelector(".close");
-    close.addEventListener('click', function() {
-        close.parentElement.remove();
-    })
+    if (close != null) {
+        close.addEventListener('click', function() {
+            close.parentElement.remove();
+        })
+    }
 })();
 
 (function alertsCSS() {
     let alertDiv = document.querySelector(".alert");
-    alertDiv.style.display = "flex";
-    alertDiv.style.justifyContent = "space-between";
+    if (alertDiv != null) {
+        alertDiv.style.display = "flex";
+        alertDiv.style.justifyContent = "space-between";
+    }
 })();
